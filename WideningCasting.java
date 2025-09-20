4a8ac940e648077505258c89198e44eb584edf2a
SakthiPrabhurajan <138732741+SakritUser123@users.noreply.github.com>
2025-09-20 12:54:25 -0400

    Create WideningCasting.java

---
+++ WideningCasting.java
@@ -0,0 +1,9 @@
+class WideningCasting {
+    public static void main(String[] args) {
+        int num = 9;
+        double numnew = num; // automatic casting int to double
+        System.out.println(num);    // Outputs 9
+        System.out.println(numnew);
+        
+    }
+}
