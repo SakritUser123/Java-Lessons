003adb748d3745481ae86c0ac9325d874c22b703
SakthiPrabhurajan <138732741+SakritUser123@users.noreply.github.com>
2025-09-20 12:39:56 -0400

    Create MoreData.java

---
+++ MoreData.java
@@ -0,0 +1,21 @@
+class MoreData {
+    public static void main(String[] args) {
+       String fname = "John ";
+       System.out.println("Hello " + fname);
+       String lname = "Doe";
+       String fullname = fname + lname;
+       System.out.println(fullname);
+       int x = 5;
+       int y = 6;
+       System.out.println(x+y);
+       System.out.println("Hi " + x + y );
+       System.out.println("Hi " + (x + y) );
+       int a = 7 , b = 8 , c = 2;
+       System.out.println(a + b + c);
+       int d , e ,f;
+       d = e = f = 60;
+       System.out.println(d + e + f);
+       
+
+    }
+}
