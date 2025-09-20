e3b0fcb896297583b1e68e7f6196ac3e2cde2a27
SakthiPrabhurajan <138732741+SakritUser123@users.noreply.github.com>
2025-09-20 12:37:36 -0400

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,15 @@
+class Main {
+    public static void main(String[] args) {
+        String name = "Sakrit";
+        int studentID = 410443;
+        int studentAge = 14;
+        float studentFee = 300.25f;
+        char studentGrade = 'A';
+        System.out.println("Student name is: " + name);
+        System.out.println("Student ID is: " + studentID);
+        System.out.println("Student Age is: " + studentAge);
+        System.out.println("Student Fee is: " + studentFee);
+        System.out.println("Student Grade is: " + studentGrade);
+        
+    }
+}
