9181d3694b73ae918986d252e23a29b04cb84663
SakthiPrabhurajan <138732741+SakritUser123@users.noreply.github.com>
2025-09-20 12:51:10 -0400

    Create Var.java

---
+++ Var.java
@@ -0,0 +1,7 @@
+class Var {
+    public static void main(String[] args) {
+        var int = 5;
+        // var can be used whne you dont specify type the computer //itself finds out what typ eit is it had ot be given a //value it cant be var x ;
+        
+    }
+}
