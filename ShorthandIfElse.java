2231272d97fa8a72e419dd8afad27affcc8c4d74
SakthiPrabhurajan <138732741+SakritUser123@users.noreply.github.com>
2025-09-21 11:43:43 -0400

    Create ShorthandIfElse.java

---
+++ ShorthandIfElse.java
@@ -0,0 +1,7 @@
+class ShorthandIfElse {
+    public static void main(String[] args) {
+        int time = 20;
+        String result = (time < 18) ? "Good day." : "Good Evening.";
+        System.out.println(result);
+    }
+}
