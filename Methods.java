00d0d5c17dcc7767a6fbb88832db0a518f8ec1d6
SakthiPrabhurajan <138732741+SakritUser123@users.noreply.github.com>
2025-09-25 17:48:23 -0400

    Create Methods.java

---
+++ Methods.java
@@ -0,0 +1,9 @@
+public Methods {
+    static int MyMethod(int x, int y) {
+        return y +x;
+    }
+    public static void main(String[] args){
+        int z = MyMethod(5,8);
+        System.out.println(z);
+    }
+}
