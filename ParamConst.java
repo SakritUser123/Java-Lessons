550b7c2da895cdc7d9ab39666adf46984b054ab4
SakthiPrabhurajan <138732741+SakritUser123@users.noreply.github.com>
2025-09-26 17:39:05 -0400

    Create ParamConst.java

---
+++ ParamConst.java
@@ -0,0 +1,19 @@
+public class ParamConst {
+    int modelYear;
+    String modelName;
+    public Main(int year, String name)
+    {
+        modelYear = year;
+        modelName = name;
+       
+    }
+    public static void main(String[]  args)
+    {
+        Main myCar = new Main(1969,"Mustang");
+        System.out.println(myCar.modelYear);
+        System.out.println(myCar.modelName);
+       
+    }
+   
+   
+}
